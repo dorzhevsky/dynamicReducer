8c1cdf3e93d88550056063fe7c5364e7739874bf
dorzhevsky <d.orzhevsky@gardatech.ru>
2023-07-13 14:05:35 +0300

    Separate attachedReducersTree from combineAttachedReducers

---
+++ src/attachedReducersTree.js
@@ -0,0 +1,63 @@
+import reduceReducers from "reduce-reducers";
+import find from "lodash/find";
+import split from "lodash/split";
+import forEach from "lodash/forEach";
+import reduce from "lodash/reduce";
+import combineReducers from "./combineReducers";
+
+const attachedReducersTree = () => {
+
+  const _createNode = (key = null) => {
+    return { key, children: [] };
+  };
+
+  const _insertNodeIfNotExists = (key, parent) => {
+    let child = find(parent.children, { key });
+    if (child) {
+      return child;
+    }
+    child = _createNode(key);
+    parent.children.push(child);
+    return child;
+  };
+
+  const _createReducer = node => {
+    if (node.children.length > 0) {
+      const reducersMap = reduce(
+        node.children,
+        (o, e) => {
+          const obj = o;
+          obj[e.key] = _createReducer(e);
+          return obj;
+        },
+        {}
+      );
+      return node.reducer
+        ? reduceReducers(node.reducer, combineReducers(reducersMap))
+        : combineReducers(reducersMap);
+    }
+    return node.reducer;
+  };
+
+  const addReducer = (key, reducer) => {
+    const splitItems = split(key, ".");
+    let parent = root;
+    forEach(splitItems, e => {
+      parent = _insertNodeIfNotExists(e, parent);
+    });
+    parent.reducer = reducer;
+  };
+
+  const createReducer = () => {
+    return _createReducer(root);
+  };
+
+  const root = _createNode();
+
+  return {
+    addReducer,
+    createReducer
+  }
+}
+
+export default attachedReducersTree;
--- src/combineAttachedReducers.js
+++ src/combineAttachedReducers.js
@@ -1,73 +1,18 @@
-import reduceReducers from "reduce-reducers";
-import find from "lodash/find";
-import split from "lodash/split";
 import forEach from "lodash/forEach";
 import keys from "lodash/keys";
-import reduce from "lodash/reduce";
 import noop from "lodash/noop";
-import combineReducers from "./combineReducers";
-
-class AttachedReducersTree {
-  constructor() {
-    this.root = this.createChild();
-  }
-
-  addReducer = (key, dynamicReducer) => {
-    const splitItems = split(key, ".");
-    let parent = this.root;
-    forEach(splitItems, e => {
-      parent = this.insert(e, parent);
-    });
-    parent.reducer = dynamicReducer;
-  };
-
-  createReducer = () => {
-    return this.createNodeReducer(this.root);
-  };
-
-  createNodeReducer = node => {
-    if (node.children.length > 0) {
-      const reducersMap = reduce(
-        node.children,
-        (o, e) => {
-          const obj = o;
-          obj[e.key] = this.createNodeReducer(e);
-          return obj;
-        },
-        {}
-      );
-      return node.reducer
-        ? reduceReducers(node.reducer, combineReducers(reducersMap))
-        : combineReducers(reducersMap);
-    }
-    return node.reducer;
-  };
-
-  insert = (key, parent) => {
-    let child = find(parent.children, { key });
-    if (child) {
-      return child;
-    }
-    child = this.createChild(key);
-    parent.children.push(child);
-    return child;
-  };
-
-  createChild = (key = null) => {
-    return { key, children: [] };
-  };
-}
+import attachedReducersTree from "./attachedReducersTree";
 
 const combineAttachedReducers = map => {
   const k = keys(map);
   if (!k.length) {
     return noop;
   }
-  const tree = new AttachedReducersTree();
+  const { addReducer, createReducer } = attachedReducersTree();
   forEach(map, (d, key) => {
-    tree.addReducer(key, d);
+    addReducer(key, d);
   });
-  return tree.createReducer();
+  return createReducer();
 };
 
 export default combineAttachedReducers;
